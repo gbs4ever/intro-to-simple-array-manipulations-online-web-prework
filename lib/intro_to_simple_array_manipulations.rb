1f35a80e0f9c532805fb46175881da0bdd5745d4
s greenbaum <gbs4ever2@gmail.com>
2018-12-17 06:18:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,8 +31,8 @@ array.flatten
 end
 
 def using_delete(array,string)
-array.delete(string)  
+array.delete(string)
   end
   def using_delete_at(array,integer)
-  array.delete(integer)  
+  array.delete(integer)
   end
