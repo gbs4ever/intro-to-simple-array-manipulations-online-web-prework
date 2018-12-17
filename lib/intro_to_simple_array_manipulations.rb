93c0aa86e99536540d67849f90428925c3117b06
s greenbaum <gbs4ever2@gmail.com>
2018-12-17 06:07:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,7 +18,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 def using_concat(array,array2)
-end  
+  array.concat(array2)
+end
 def using_insert(array, element)
-array.insert(3,element)  
-  end  
+array.insert(3,element)
+  end
